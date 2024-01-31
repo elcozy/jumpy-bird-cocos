4a8d88a094646b79ba9f81e6b0622892170367b0
Chiemeka Madufor <61386343+elcozy@users.noreply.github.com>
2024-01-31 23:30:46 +0100

    upload build

---
+++ docs/index.js
@@ -0,0 +1,35 @@
+System.register(["./application.js"], function (_export, _context) {
+    "use strict";
+
+    var Application, canvas, $p, bcr, application;
+
+    function topLevelImport(url) {
+        return System["import"](url);
+    }
+
+    return {
+        setters: [
+            function (_applicationJs) {
+                Application = _applicationJs.Application;
+            },
+        ],
+        execute: function () {
+            canvas = document.getElementById("GameCanvas");
+            $p = canvas.parentElement;
+            bcr = $p.getBoundingClientRect();
+            // canvas.width = 640;
+            // canvas.height = 960;
+            application = new Application();
+            topLevelImport("cc")
+                .then(function (engine) {
+                    return application.init(engine);
+                })
+                .then(function () {
+                    return application.start();
+                })
+                ["catch"](function (err) {
+                    console.error(err);
+                });
+        },
+    };
+});
