badab1b470f3e6dbd4016948d59f6f9da895ee6c
Chiemeka Madufor <61386343+elcozy@users.noreply.github.com>
2024-01-31 23:40:38 +0100

    update docs

--- docs/index.js
+++ docs/index.js
@@ -1,35 +1,30 @@
 System.register(["./application.js"], function (_export, _context) {
-    "use strict";
+  "use strict";
 
-    var Application, canvas, $p, bcr, application;
+  var Application, canvas, $p, bcr, application;
 
-    function topLevelImport(url) {
-        return System["import"](url);
+  function topLevelImport(url) {
+    return System["import"](url);
+  }
+
+  return {
+    setters: [function (_applicationJs) {
+      Application = _applicationJs.Application;
+    }],
+    execute: function () {
+      canvas = document.getElementById('GameCanvas');
+      $p = canvas.parentElement;
+      bcr = $p.getBoundingClientRect();
+      canvas.width = bcr.width;
+      canvas.height = bcr.height;
+      application = new Application();
+      topLevelImport('cc').then(function (engine) {
+        return application.init(engine);
+      }).then(function () {
+        return application.start();
+      })["catch"](function (err) {
+        console.error(err);
+      });
     }
-
-    return {
-        setters: [
-            function (_applicationJs) {
-                Application = _applicationJs.Application;
-            },
-        ],
-        execute: function () {
-            canvas = document.getElementById("GameCanvas");
-            $p = canvas.parentElement;
-            bcr = $p.getBoundingClientRect();
-            // canvas.width = 640;
-            // canvas.height = 960;
-            application = new Application();
-            topLevelImport("cc")
-                .then(function (engine) {
-                    return application.init(engine);
-                })
-                .then(function () {
-                    return application.start();
-                })
-                ["catch"](function (err) {
-                    console.error(err);
-                });
-        },
-    };
+  };
 });
